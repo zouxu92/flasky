e05c2e8610d5e2123d56d28c56e6d625fbeaf6c3
zouxu_PC <zouxu_PC>
2016-11-10 18:19:13 +0800

    穿件第一个hellworld 程序

---
+++ hello.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-* -*-
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return '<h1>Hello World!</h1>'
+
+if __name__ == '__main__':
+	app.run(debug=True)
