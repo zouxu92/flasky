9c6998e85b97571135a29338567f258e41a72ac5
zouxu9203 <zouxu9203@163.com>
2016-11-15 11:59:47 +0800

    时间戳flask-moment模板

--- hello.py
+++ hello.py
@@ -2,13 +2,19 @@
 
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+# 初始化flask_moment --> 本地时间
+moment = Moment(app)
+
 
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('index.html',
+						   current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
@@ -23,6 +29,7 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
 	return render_template('500.html'), 500
+
 
 
 if __name__ == '__main__':
