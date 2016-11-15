1621a7ed9b52cc36ff547a461cde9c7857d5f9c1
zouxu9203 <zouxu9203@163.com>
2016-11-15 15:36:45 +0800

    提交表单相关

--- hello.py
+++ hello.py
@@ -4,16 +4,34 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+
+# 表单
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import InputRequired, Required
+
 app = Flask(__name__)
+# 设置Flask-WTF
+app.config['SECRET_KEY'] = 'hard to guess string' # 保证安全不应该写入这里,写在配置文件中
 bootstrap = Bootstrap(app)
 # 初始化flask_moment --> 本地时间
 moment = Moment(app)
 
+# 定义表单类
+class NameForm(Form):
+	name = StringField('What is your name?', validators=[InputRequired()])
+	submit = SubmitField('Submit')
 
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html',
+	name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	return render_template('index.html', form=form, name=name,
 						   current_time=datetime.utcnow())
 
 
